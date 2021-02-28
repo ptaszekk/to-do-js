a6097b96de6ea2c47dc666b3fd411916da2f1604
ptaszekk <ptaszekjakub@gmail.com>
2021-02-28 16:33:30 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-
 const form = document.querySelector('form');
 const input = document.querySelector('#taskInput');
 const addBtn = document.querySelector('#add-btn');
 const clearBtn = document.querySelector('#clear-btn');
-const tasksTable = document.querySelector('#tasksTable')
+const tasksTable = document.querySelector('#tasksTable');
 const ul = document.createElement('ul');
 
 const storage = window.localStorage;
@@ -12,81 +11,77 @@ const allTasks = storage.getItem('tasks') ? JSON.parse(storage.getItem('tasks'))
 storage.setItem('tasks', JSON.stringify(allTasks));
 
 function addTask(e) {
-    e.preventDefault();
-    
-    if(!input.value){
-        return null
-    } else { 
-        let newTask = {
-            value: input.value,
-            id: Date.now(),
-        };
-        
-        let tempArr = JSON.parse(storage.getItem('tasks'));
-        tempArr.push(newTask)
-        storage.setItem('tasks', JSON.stringify(tempArr));
-        input.value = '';
-    }
-    displayTasks();
+	e.preventDefault();
+
+	if (!input.value) {
+		alert('please enter task');
+	} else {
+		let newTask = {
+			value: input.value,
+			id: Date.now(),
+		};
+
+		let tempArr = JSON.parse(storage.getItem('tasks'));
+		tempArr.push(newTask);
+		storage.setItem('tasks', JSON.stringify(tempArr));
+		input.value = '';
+	}
+	displayTasks();
 }
 
-
 function displayTasks() {
-    let parsedTasks =  JSON.parse(storage.getItem('tasks'))
-    
-    ul.innerHTML='';
-    
-    parsedTasks.map(task => {
-        
-        const li = document.createElement('li');
-        const deleteBtn = document.createElement('button');
-        
-        deleteBtn.addEventListener('click', deleteTask )
-        
-        li.innerHTML= task.value;
-        
-        deleteBtn.innerHTML = 'delete';
-      
-        ul.classList.add('tasks-list');
-        li.classList.add('task-item');
-        li.id = task.id;
+	let parsedTasks = JSON.parse(storage.getItem('tasks'));
 
-        deleteBtn.classList.add('delete-btn');
-        
-        
-        tasksTable.appendChild(ul);
-        ul.append(li);
-        li.append( deleteBtn)
-    });
-};
+	ul.innerHTML = '';
+
+	parsedTasks.map((task) => {
+		const li = document.createElement('li');
+		const deleteBtn = document.createElement('button');
+
+		deleteBtn.addEventListener('click', deleteTask);
+
+		li.innerHTML = task.value;
+
+		deleteBtn.innerHTML = 'delete';
+
+		ul.classList.add('tasks-list');
+		li.classList.add('task-item');
+		li.id = task.id;
+		li.setAttribute('contenteditable', 'true');
+
+		deleteBtn.classList.add('delete-btn');
+
+		tasksTable.appendChild(ul);
+		ul.append(li);
+		li.append(deleteBtn);
+	});
+}
 
 displayTasks();
 
 function deleteTask() {
-    
-    let tasksLocal  = JSON.parse(storage.getItem('tasks'));
-    let id = this.parentElement.getAttribute('id');
-   
-    const tempArr = tasksLocal.filter(task=> task.id != id);
-    console.log(tempArr);
-    storage.setItem('tasks', JSON.stringify(tempArr));
-    console.log(storage);
+	let tasksLocal = JSON.parse(storage.getItem('tasks'));
+	let id = this.parentElement.getAttribute('id');
 
-    this.parentElement.classList.add('animation');
+	const tempArr = tasksLocal.filter((task) => task.id != id);
+	console.log(tempArr);
+	storage.setItem('tasks', JSON.stringify(tempArr));
+	console.log(storage);
 
-    setTimeout(()=>{
-        var parentList = this.parentElement.parentElement;
-        parentList.removeChild(this.parentElement);
-    }, 500)
+	this.parentElement.classList.add('animation');
+
+	setTimeout(() => {
+		var parentList = this.parentElement.parentElement;
+		parentList.removeChild(this.parentElement);
+	}, 700);
 }
 
 function clear() {
-    storage.clear();
-    allTasks.length = 0;
-    console.log(storage);
-    ul.innerHTML = 'you have no tasks';
+	storage.clear();
+	allTasks.length = 0;
+	console.log(storage);
+	ul.innerHTML = 'you have no tasks';
 }
 
-addBtn.addEventListener('click', addTask )
-clearBtn.addEventListener('click', clear )
-
+addBtn.addEventListener('click', addTask);
+clearBtn.addEventListener('click', clear);
